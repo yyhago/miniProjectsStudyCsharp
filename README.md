385d6cefd3c3b7ae637fab5db8d1cf0de2f4c63b
yyhago <yhago.felipe.teles@gmail.com>
2024-12-17 18:28:34 -0300

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projetos de Estudo em C#
+
+Bem-vindo ao meu repositório de estudos em C#! Aqui você encontrará diversos projetos práticos, criados com o objetivo de aprimorar meu conhecimento na linguagem e explorar novos conceitos de programação.
+
+Cada projeto é uma oportunidade de crescimento, com desafios que me ajudam a fortalecer habilidades e a compreender melhor a teoria e a prática de C#.
+
+Sinta-se à vontade para explorar, contribuir ou se inspirar!
--- calculadoraDesconto/obj/Debug/net9.0/calculadoraDesconto.AssemblyInfo.cs
+++ calculadoraDesconto/obj/Debug/net9.0/calculadoraDesconto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadoraDesconto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736443595a37e12d0ed494dc3146771c29ccda60")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadoraDesconto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadoraDesconto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
